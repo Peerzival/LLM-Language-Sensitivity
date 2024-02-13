6f0e7515ff5c32d9b8a101b8108bba32fbabac9b
DerHeldDerSteineFanboy <70917277+DerHeldDerSteineFanboy@users.noreply.github.com>
2024-02-13 12:19:59 +0100

    Delete unnecessary image files and update requirements.txt

--- Paraphasing/plotting_service.py
+++ Paraphasing/plotting_service.py
@@ -4,11 +4,15 @@ import matplotlib.gridspec as gridspec
 import json
 import os
 from pathlib import Path
+from rouge import Rouge
+from bert_score import score
+import torch
 
 dataset_id = [0, 1, 2]
 file_names = ['social_iq_score_set.json',
               'coqa_score_set.json', 'com2sense_score_set.json']
 dataset_names = ['SocialIQa', 'CoQA', 'COM2SENSE']
+quality_dimensions = ['veryLow', 'low', 'medium', 'high', 'veryHigh']
 
 for id in dataset_id:
     result_set_file_path = os.path.join(
@@ -21,48 +25,18 @@ for id in dataset_id:
 
     dataset_name = dataset_names[id]
 
-    def get_rougeL_scores() -> dict:
-        rougeL_scores = {
-            "veryLow": "",
-            "low": "",
-            "medium": "",
-            "high": "",
-            "veryHigh": ""
-        }
-
-        veryLow: float = 0.0
-        low: float = 0.0
-        medium: float = 0.0
-        high: float = 0.0
-        veryHigh: float = 0.0
-
-        # for item in data[dataset_name]['ROUGE']:
-        #   if item['id'] == id and item['model_parameter_group'] == "low":
-        #     rougeL_scores['low'] = float(item['metric']['rougeL']['f1'])
-        #   elif item['id'] == id and item['model_parameter_group'] == "medium":
-        #     rougeL_scores['medium'] = float(item['metric']['rougeL']['f1'])
-        #   elif item['id'] == id and item['model_parameter_group'] == "high":
-        #     rougeL_scores['high'] = float(item['metric']['rougeL']['f1'])
-
-        for item in data[dataset_name]['ROUGE']:
-            if item['model_parameter_group'] == "veryLow":
-                veryLow += item['metric']['rougeL']['f1']
-            elif item['model_parameter_group'] == "low":
-                low += item['metric']['rougeL']['f1']
-            elif item['model_parameter_group'] == "medium":
-                medium += item['metric']['rougeL']['f1']
-            elif item['model_parameter_group'] == "high":
-                high += item['metric']['rougeL']['f1']
-            elif item['model_parameter_group'] == "veryHigh":
-                veryHigh += item['metric']['rougeL']['f1']
-
-        rougeL_scores['veryLow'] = veryLow / len(data[dataset_name]['ROUGE'])
-        rougeL_scores['low'] = low / len(data[dataset_name]['ROUGE'])
-        rougeL_scores['medium'] = medium / len(data[dataset_name]['ROUGE'])
-        rougeL_scores['high'] = high / len(data[dataset_name]['ROUGE'])
-        rougeL_scores['veryHigh'] = veryHigh / len(data[dataset_name]['ROUGE'])
-
-        return rougeL_scores
+    def calculate_average_rouge_l():
+        rouge = Rouge()
+        values = []
+        for dimension in quality_dimensions:
+            hypothesis_sentences = [item['reference_text']
+                                    for item in data[dataset_name]['ROUGE'] if item['model_parameter_group'] == dimension]
+            reference_sentences = [item['candidate_text']
+                                   for item in data[dataset_name]['ROUGE'] if item['model_parameter_group'] == dimension]
+            scores = rouge.get_scores(
+                hypothesis_sentences, reference_sentences, avg=True)
+            values.append(scores['rouge-l']['f'])
+        return values
 
     def get_bleu_scores() -> dict:
         bleu_scores = {
@@ -73,102 +47,61 @@ for id in dataset_id:
             "veryHigh": ""
         }
 
-        veryLow: float = 0.0
-        low: float = 0.0
-        medium: float = 0.0
-        high: float = 0.0
-        veryHigh: float = 0.0
-
-        # for item in data[dataset_name]['BLEU']:
-        #   if item['id'] == id and item['model_parameter_group'] == "low":
-        #     bleu_scores['low'] = float(item['metric']['bleu']['score'])
-        #   elif item['id'] == id and item['model_parameter_group'] == "medium":
-        #     bleu_scores['medium'] = float(item['metric']['bleu']['score'])
-        #   elif item['id'] == id and item['model_parameter_group'] == "high":
-        #     bleu_scores['high'] = float(item['metric']['bleu']['score'])
+        veryLow = []
+        low = []
+        medium = []
+        high = []
+        veryHigh = []
 
         for item in data[dataset_name]['BLEU']:
             if item['model_parameter_group'] == "veryLow":
-                veryLow += item['metric']['bleu']['score']
+                veryLow.append(item['metric']['bleu']['score'])
             elif item['model_parameter_group'] == "low":
-                low += item['metric']['bleu']['score']
+                low.append(item['metric']['bleu']['score'])
             elif item['model_parameter_group'] == "medium":
-                medium += item['metric']['bleu']['score']
+                medium.append(item['metric']['bleu']['score'])
             elif item['model_parameter_group'] == "high":
-                high += item['metric']['bleu']['score']
+                high.append(item['metric']['bleu']['score'])
             elif item['model_parameter_group'] == "veryHigh":
-                veryHigh += item['metric']['bleu']['score']
+                veryHigh.append(item['metric']['bleu']['score'])
 
-        bleu_scores['veryLow'] = veryLow / len(data[dataset_name]['BLEU'])
-        bleu_scores['low'] = low / len(data[dataset_name]['BLEU'])
-        bleu_scores['medium'] = medium / len(data[dataset_name]['BLEU'])
-        bleu_scores['high'] = high / len(data[dataset_name]['BLEU'])
-        bleu_scores['veryHigh'] = veryHigh / len(data[dataset_name]['BLEU'])
+        bleu_scores['veryLow'] = sum(veryLow) / len(veryLow)
+        bleu_scores['low'] = sum(low) / len(low)
+        bleu_scores['medium'] = sum(medium) / len(medium)
+        bleu_scores['high'] = sum(high) / len(high)
+        bleu_scores['veryHigh'] = sum(veryHigh) / len(veryHigh)
 
         return bleu_scores
 
     def get_bert_score() -> dict:
-        bert_scores = {
-            "veryLow": "",
-            "low": "",
-            "medium": "",
-            "high": "",
-            "veryHigh": ""
-        }
+        values = []
+        for dimension in quality_dimensions:
+            hypothesis_sentences = [item['reference_text']
+                                    for item in data[dataset_name]['ROUGE'] if item['model_parameter_group'] == dimension]
+            reference_sentences = [item['candidate_text']
+                                   for item in data[dataset_name]['ROUGE'] if item['model_parameter_group'] == dimension]
+            P, R, F1 = score(hypothesis_sentences,
+                             reference_sentences, lang='en', verbose=False)
+            average_F1 = torch.mean(F1)
+            values.append(average_F1)
 
-        veryLow: float = 0.0
-        low: float = 0.0
-        medium: float = 0.0
-        high: float = 0.0
-        veryHigh: float = 0.0
-
-        # for item in data[dataset_name]['BERTScore']:
-        #   if item['id'] == id and item['model_parameter_group'] == "low":
-        #     bert_scores['low'] = float(item['metric']['bertscore']['f1'])
-        #   elif item['id'] == id and item['model_parameter_group'] == "medium":
-        #     bert_scores['medium'] = float(item['metric']['bertscore']['f1'])
-        #   elif item['id'] == id and item['model_parameter_group'] == "high":
-        #     bert_scores['high'] = float(item['metric']['bertscore']['f1'])
-
-        for item in data[dataset_name]['BERTScore']:
-            if item['model_parameter_group'] == "veryLow":
-                veryLow += float(item['metric']['bertscore']['f1'])
-            elif item['model_parameter_group'] == "low":
-                low += float(item['metric']['bertscore']['f1'])
-            elif item['model_parameter_group'] == "medium":
-                medium += float(item['metric']['bertscore']['f1'])
-            elif item['model_parameter_group'] == "high":
-                high += float(item['metric']['bertscore']['f1'])
-            elif item['model_parameter_group'] == "veryHigh":
-                veryHigh += float(item['metric']['bertscore']['f1'])
-
-        bert_scores['veryLow'] = veryLow / len(data[dataset_name]['BERTScore'])
-        bert_scores['low'] = low / len(data[dataset_name]['BERTScore'])
-        bert_scores['medium'] = medium / len(data[dataset_name]['BERTScore'])
-        bert_scores['high'] = high / len(data[dataset_name]['BERTScore'])
-        bert_scores['veryHigh'] = veryHigh / \
-            len(data[dataset_name]['BERTScore'])
-
-        return bert_scores
-
-    x = ['very low', 'low', 'medium', 'high', 'very high']
-    # x = ['low', 'medium', 'high']
+        return values
 
     # Plot the scores
     plt.figure(figsize=(10, 5))
     gs = gridspec.GridSpec(nrows=2, ncols=2)
 
     ax0 = plt.subplot(gs[0, 0])
-    ax0.plot(x, list(get_rougeL_scores().values()))
+    ax0.plot(quality_dimensions, calculate_average_rouge_l())
     ax0.set_title('Avrg. ROUGE-L F1')
     # ax0.set_xlabel('Model Parameter Group')
 
     ax1 = plt.subplot(gs[1, 0])
-    ax1.plot(x, list(get_bleu_scores().values()))
+    ax1.plot(quality_dimensions, list(get_bleu_scores().values()))
     ax1.set_title('Avrg. BLEU')
 
     ax2 = plt.subplot(gs[:, 1])
-    ax2.plot(x, list(get_bert_score().values()))
+    ax2.plot(quality_dimensions, get_bert_score())
     ax2.set_title('Avrg. BERTScore F1')
 
     plt.subplots_adjust(top=0.85, bottom=0.1, left=0.10,
